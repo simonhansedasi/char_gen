c2d9c0fea26f5ed7419221e1e77932a023c7aa3d
Simon Hans Edasi <woses21@gmail.com>
2024-11-06 09:45:45 -0800

    updating app to return backstory after main stats

--- app.py
+++ app.py
@@ -11,50 +11,51 @@ def add_security_headers(response):
     response.headers['Permissions-Policy'] = 'interest-cohort=()'  # Disable FLoC
     return response
 
-@app.route('/generate_character', methods=['GET', 'POST'])
+@app.route('/generate_character', methods=['POST'])
 def generate_character():
-    # if request.method == 'POST':
-        # Run character generator logic when form is submitted
-    dead_farmers = 0
-    recommended_species = None
-    chosen_class = None
     background = None
+    species = None
+    chosen_class = None
+    
+    dead_farmers = 0
 
-    while not (recommended_species and chosen_class and background):
+    # Main character generation without background
+    while not (species and chosen_class and background):
         stats, attempts = g.roll_stats()
         dead_farmers += attempts
-        recommended_species = g.recommend_species(stats)
-        updated_stats = g.apply_species_bonus(stats, recommended_species)
+        species = g.recommend_species(stats)
+        updated_stats = g.apply_species_bonus(stats, species)
         optimal_stats = g.sort_stats(updated_stats)
         chosen_class = g.select_class(optimal_stats)
         background = g.pick_background(optimal_stats)
-    
-    # printed_stats = {new_key: updated_stats[old_key] for new_key, old_key in zip(g.stats, updated_stats.keys())}
-    printed_stats = OrderedDict([
-        ("STR", updated_stats[0]),
-        ("DEX", updated_stats[1]),
-        ("CON", updated_stats[2]),
-        ("INT", updated_stats[3]),
-        ("WIS", updated_stats[4]),
-        ("CHA", updated_stats[5])
-    ])
-    # Generate the background text
-    character_background = g.generate_background(
-        recommended_species, chosen_class, background, updated_stats, dead_farmers
-    )
-    character_background = character_background.replace('\n', '<br>')
-    # print(character_background)
-    # If needed, return JSON for the GET request
-    return jsonify({
-        'stats': printed_stats,
-        'species': recommended_species,
+
+    # Respond with essential data first
+    partial_response = jsonify({
+        'stats': updated_stats,
+        'species': species,
         'class': chosen_class,
         'background': background,
-        'character_background': character_background
+        'character_background': "Generating character background...",
     })
 
+    # Return this partial data while background is generated in the background
+    return partial_response
     # # If GET request, show empty form
     # return render_template('index.html')
+    
 
+@app.route('/generate_background', methods=['POST'])
+def generate_background():
+    species = request.json.get('species')
+    chosen_class = request.json.get('class')
+    stats = request.json.get('stats')
+    dead_farmers = request.json.get('dead_farmers')
+    background = request.json.get('background')
+
+    # Generate background based on character data
+    character_background = g.generate_background(species, chosen_class, background, stats, dead_farmers)
+    # character_background = character_background.replace('\n', '<br>')
+
+    return jsonify({'character_background': character_background})
 if __name__ == '__main__':
     app.run(debug=True)
